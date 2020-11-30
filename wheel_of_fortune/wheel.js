2c94c49d18262923c638c3d57217e183717425af
octavianhokela <octavian@hokela.co.ke>
2020-11-30 19:22:28 +0300

    Adding Safaricom colours to the wheel

--- wheel_of_fortune/wheel.js
+++ wheel_of_fortune/wheel.js
@@ -155,33 +155,14 @@ let theWheel = new Winwheel({
     'textFontSize'    : 24,         // Set default font size for the segments.
     'textOrientation' : 'vertical', // Make text vertial so goes down from the outside of wheel.
     'textAlignment'   : 'outer',    // Align text to outside of wheel.
-    'numSegments'     : 24,         // Specify number of segments.
+    'numSegments'     : 5,         // Specify number of segments.
     'segments'        :             // Define segments including colour and text.
         [                               // font size and test colour overridden on backrupt segments.
-            {'fillStyle' : '#ee1c24', 'text' : '300'},
-            {'fillStyle' : '#3cb878', 'text' : '450'},
-            {'fillStyle' : '#f6989d', 'text' : '600'},
-            {'fillStyle' : '#00aef0', 'text' : '750'},
-            {'fillStyle' : '#f26522', 'text' : '500'},
-            {'fillStyle' : '#000000', 'text' : 'BANKRUPT', 'textFontSize' : 16, 'textFillStyle' : '#ffffff'},
-            {'fillStyle' : '#e70697', 'text' : '3000'},
-            {'fillStyle' : '#fff200', 'text' : '600'},
-            {'fillStyle' : '#f6989d', 'text' : '700'},
-            {'fillStyle' : '#ee1c24', 'text' : '350'},
-            {'fillStyle' : '#3cb878', 'text' : '500'},
-            {'fillStyle' : '#f26522', 'text' : '800'},
-            {'fillStyle' : '#a186be', 'text' : '300'},
-            {'fillStyle' : '#fff200', 'text' : '400'},
-            {'fillStyle' : '#00aef0', 'text' : '650'},
-            {'fillStyle' : '#ee1c24', 'text' : '1000'},
-            {'fillStyle' : '#f6989d', 'text' : '500'},
-            {'fillStyle' : '#f26522', 'text' : '400'},
-            {'fillStyle' : '#3cb878', 'text' : '900'},
-            {'fillStyle' : '#000000', 'text' : 'BANKRUPT', 'textFontSize' : 16, 'textFillStyle' : '#ffffff'},
-            {'fillStyle' : '#a186be', 'text' : '600'},
-            {'fillStyle' : '#fff200', 'text' : '700'},
-            {'fillStyle' : '#00aef0', 'text' : '800'},
-            {'fillStyle' : '#ffffff', 'text' : 'LOOSE TURN', 'textFontSize' : 12}
+            {'fillStyle' : '#fff', 'text' : '300'},
+            {'fillStyle' : '#009a3e', 'text' : '450'},
+            {'fillStyle' : '#fff', 'text' : '600'},
+            {'fillStyle' : '#3bb26b', 'text' : '750'},
+            {'fillStyle' : '#e31119', 'text' : 'BANKRUPT', 'textFontSize' : 16, 'textFillStyle' : '#000'},
         ],
     'animation' :           // Specify the animation to use.
         {
@@ -194,7 +175,7 @@ let theWheel = new Winwheel({
         },
     'pins' :				// Turn pins on.
         {
-            'number'     : 24,
+            'number'     : 5,
             'fillStyle'  : 'silver',
             'outerRadius': 4,
         }
